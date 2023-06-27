2e1b78c4691bd70bdd1d4bb845d22be46a273242
Edwin Yu <edwinyyyu@gmail.com>
2023-06-27 00:40:04 +0000

    Implement job submission

--- float-submit.py
+++ float-submit.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python3
 import sys
 import subprocess
+import re
 import yaml
 
 from snakemake.utils import read_job_properties
 
+CONFIG_FILE = 'snakemake-float.yaml'
+
 
 class FloatSubmitter:
-    required_kwargs = ('address', 'username', 'password', 'nfs')
+    required_kwargs = ('address', 'username', 'password', 'dataVolume')
 
     def __init__(self, **kwargs):
-        if all(arg in kwargs for arg in self.required_kwargs):
-            self.kwargs = kwargs
-        else:
-            raise TypeError
+        for kwarg in self.required_kwargs:
+            if kwarg not in kwargs:
+                raise TypeError(f"{CONFIG_FILE} missing required: '{kwarg}'")
+
+        self.kwargs = kwargs
 
     def submit_job(self, job_file):
         cmd = ['float', 'submit']
 
         for key, value in self.kwargs.items():
-            cmd.extend([f'--{key}', value])
+            if key != 'common-extra':
+                cmd.extend([f'--{key}', value])
 
+        cmd.extend(['--image', 'cactus'])
+        cmd.extend(['--cpu', '2'])
+        cmd.extend(['--mem', '4'])
         cmd.extend(['--job', job_file])
 
-        subprocess.run(cmd)
+        output = subprocess.check_output(cmd).decode()
+        jobid = output[len('id: '): output.index('\n')]
+        return jobid
 
 
 if __name__ == '__main__':
     jobscript = sys.argv[-1]
     job_properties = read_job_properties(jobscript)
 
-    config_file = 'snakemake-float.yaml'
-
-    with open(config_file) as cf:
+    with open(CONFIG_FILE) as cf:
         float_config = yaml.safe_load(cf)
 
     float_submitter = FloatSubmitter(**float_config)
-    float_submitter.submit_job(jobscript)
+    jobid = float_submitter.submit_job(jobscript)
+    print(jobid)

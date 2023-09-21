07a20c44ed0b41b3bfddcc931f4e805a93567062
Edwin Yu <edwinyyyu@gmail.com>
2023-09-21 10:58:12 -0700

    Add some utilities for I/O

--- snakemake_executor_plugin_mmcloud/__init__.py
+++ snakemake_executor_plugin_mmcloud/__init__.py
@@ -1,13 +1,20 @@
+import hashlib
 import json
+import os
+import shutil
 import subprocess
+import tarfile
+import tempfile
 from dataclasses import dataclass, field
 from typing import Generator, List, Optional
 
-from snakemake_interface_executor_plugins import CommonSettings, ExecutorSettingsBase
+from snakemake.remote.S3 import S3Helper
+from snakemake_interface_common.exceptions import WorkflowError
 from snakemake_interface_executor_plugins.executors.base import SubmittedJobInfo
 from snakemake_interface_executor_plugins.executors.remote import RemoteExecutor
 from snakemake_interface_executor_plugins.jobs import JobExecutorInterface
 from snakemake_interface_executor_plugins.logging import LoggerExecutorInterface
+from snakemake_interface_executor_plugins.settings import CommonSettings, ExecutorSettingsBase
 from snakemake_interface_executor_plugins.workflow import WorkflowExecutorInterface
 
 from snakemake_executor_plugin_mmcloud.common import Status, async_check_output, status_map
@@ -87,10 +94,12 @@ class Executor(RemoteExecutor):
             # specify initial amount of seconds to sleep before checking for job status
             init_sleep_seconds=0,
         )
+
+        self.s3_helper = S3Helper()
+        self._response_format = ["--format", "json"]
+
         # access workflow
         self.workflow
-        # access executor specific settings
-        self.workflow.executor_settings
 
         # IMPORTANT: in your plugin, only access methods and properties of
         # Snakemake objects (like Workflow, Persistence, etc.) that are
@@ -109,26 +118,39 @@ class Executor(RemoteExecutor):
         # In case of errors outside of jobs, please raise a WorkflowError
 
     def run_job(self, job: JobExecutorInterface):
-        # Implement here how to run a job.
-        # You can access the job's resources, etc.
-        # via the job object.
-        # After submitting the job, you have to call
-        # self.report_job_submission(job_info).
-        # with job_info being of type
-        # snakemake_interface_executor_plugins.executors.base.SubmittedJobInfo.
-        # If required, make sure to pass the job's id to the job_info object, as keyword
-        # argument 'external_job_id'.
+        # job.name
+        # job.jobid
+        # job.logfile_suggestion
+        # job.is_group
+        # job.log_info
+        # job.log_error
+        # job.remote_existing_output
+        # job.download_remote_input
+        # job.properties
+        # job.resources
+        # job.check_protected_output
+        # job.is_local
+        # job.is_branched
+        # job.is_updated
+        # job.output
+        # job.register
+        # job.postprocess
+        # job.get_target_spec
+        # job.rules
+        # job.attempt
+        # job.input
+        # job.threads
+        # job.log
+        # job.cleanup
+        # job.get_wait_for_files
+        # job.format_wildcards
+        # job.is_containerized
 
-        ...
+        job_info = SubmittedJobInfo(job=job, external_jobid=jobid)
+        self.report_job_submission(job_info)
 
     async def check_active_jobs(self, active_jobs: List[SubmittedJobInfo]) -> Generator[SubmittedJobInfo, None, None]:
         # Check the status of active jobs.
-
-        # self.report_job_success(active_job).
-        # self.report_job_error(active_job).
-        # Jobs that are still running have to be yielded.
-        # To modify the time until the next call of this method,
-        # you can set self.next_sleep_seconds here.
         show_command = [
             "float",
             "show",
@@ -143,7 +165,7 @@ class Executor(RemoteExecutor):
                     try:
                         show_response = await async_check_output(*(show_command + ["--job", jobid]))
                         show_response = json.loads(show_response.decode())
-                        status = status_map(show_response["status"])
+                        job_status = status_map(show_response["status"])
                     except subprocess.CalledProcessError as e:
                         self.logger.exception(
                             f"Failed to get show response for MMCloud job: {jobid}\n"
@@ -158,11 +180,11 @@ class Executor(RemoteExecutor):
                         self.logger.exception(f"Failed to obtain status for MMCloud job: {jobid}")
                         raise
 
-                if status is Status.RUNNING:
+                if job_status is Status.RUNNING:
                     yield job
-                elif status is Status.SUCCESS:
+                elif job_status is Status.SUCCESS:
                     self.report_job_success(job)
-                elif status is Status.FAILURE:
+                elif job_status is Status.FAILURE:
                     self.report_job_error(job)
         except subprocess.CalledProcessError:
             self.logger.exception("Failed to obtain check active MMCloud jobs")
@@ -205,3 +227,67 @@ class Executor(RemoteExecutor):
                 raise
 
             self.logger.info("Logged in to OpCenter")
+
+    # from snakemake_executor_plugin_google_lifesciences
+    def _set_workflow_sources(self):
+        """
+        We only add files from the working directory that are config related
+        (e.g., the Snakefile or a config.yml equivalent), or checked into git.
+        """
+        self.workflow_sources = []
+
+        for wfs in self.dag.get_sources():
+            if os.path.isdir(wfs):
+                for dirpath, dirnames, filenames in os.walk(wfs):
+                    self.workflow_sources.extend([self.check_source_size(os.path.join(dirpath, f)) for f in filenames])
+            else:
+                self.workflow_sources.append(self.check_source_size(os.path.abspath(wfs)))
+
+    # from snakemake_executor_plugin_google_lifesciences
+    def _generate_build_source_package(self):
+        """
+        In order for the instance to access the working directory in storage,
+        we need to upload it. This file is cleaned up at the end of the run.
+        We do this, and then obtain from the instance and extract.
+        """
+        # Workflow sources for cloud executor must all be under same workdir root
+        for filename in self.workflow_sources:
+            if self.workdir not in os.path.realpath(filename):
+                raise WorkflowError(
+                    "All source files must be present in the working directory, "
+                    "{workdir} to be uploaded to a build package that respects "
+                    "relative paths, but {filename} was found outside of this "
+                    "directory. Please set your working directory accordingly, "
+                    "and the path of your Snakefile to be relative to it.".format(
+                        workdir=self.workdir, filename=filename
+                    )
+                )
+
+        # We will generate a tar.gz package, renamed by hash
+        tmpname = next(tempfile._get_candidate_names())
+        targz = os.path.join(tempfile.gettempdir(), "snakemake-%s.tar.gz" % tmpname)
+        tar = tarfile.open(targz, "w:gz")
+
+        # Add all workflow_sources files
+        for filename in self.workflow_sources:
+            arcname = filename.replace(self.workdir + os.path.sep, "")
+            tar.add(filename, arcname=arcname)
+
+        tar.close()
+
+        # Rename based on hash, in case user wants to save cache
+        hasher = hashlib.sha256()
+        hasher.update(open(targz, "rb").read())
+        sha256 = hasher.hexdigest()
+        hash_tar = os.path.join(self.workflow.persistence.aux_path, f"workdir-{sha256}.tar.gz")
+
+        # Only copy if we don't have it yet, clean up if we do
+        if not os.path.exists(hash_tar):
+            shutil.move(targz, hash_tar)
+        else:
+            os.remove(targz)
+
+        # We will clean these all up at shutdown
+        self._build_packages.add(hash_tar)
+
+        return hash_tar
